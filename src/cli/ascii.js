f9fc2ea4958a075b3fdc6c9fe1260ff2167d8941
J3rry320 <rutu.satpathy44@gmail.com>
2025-01-08 17:13:16 +0530

    Added more arts from gpt

--- src/cli/ascii.js
+++ src/cli/ascii.js
@@ -1,15 +1,6 @@
 const figlet = require("figlet");
 const cliColor = require("cli-color");
-const winston = require("winston");
 
-const logger = winston.createLogger({
-  level: "info",
-  format: winston.format.combine(
-    winston.format.colorize(),
-    winston.format.simple()
-  ),
-  transports: [new winston.transports.Console()],
-});
 
 function generateAsciiArt(text, font, color) {
   return cliColor[color](figlet.textSync(text, { font: font }));
@@ -21,8 +12,19 @@ const art3 = generateAsciiArt("ThemeMaker", "Big", "magenta");
 const art4 = generateAsciiArt("ThemeMaker", "Small", "yellow");
 const art5 = generateAsciiArt("ThemeMaker", "Banner", "red");
 const art6 = generateAsciiArt("ThemeMaker", "Ghost", "blue");
-
-const arts = [art1, art2, art3, art4, art5, art6];
+const art7 = generateAsciiArt("ThemeMaker", "Lean", "white");
+const art8 = generateAsciiArt("ThemeMaker", "Star Wars", "cyan");
+const art9 = generateAsciiArt("ThemeMaker", "Bulbhead", "green");
+const art10 = generateAsciiArt("ThemeMaker", "Acrobatic", "yellow");
+const art11 = generateAsciiArt("ThemeMaker", "Blocks", "blue");
+const art12 = generateAsciiArt("ThemeMaker", "Digital", "red");
+const art13 = generateAsciiArt("ThemeMaker", "Epic", "magenta");
+const art14 = generateAsciiArt("ThemeMaker", "Roman", "white");
+const art15 = generateAsciiArt("ThemeMaker", "Isometric1", "cyan");
+const art16 = generateAsciiArt("ThemeMaker", "Cyberlarge", "green");
+const arts = new Array(16)
+  .fill(null)
+  .map((_, index) => eval(`art${index + 1}`));
 
 // Function to load random ASCII art
 function displayRandomArt() {
